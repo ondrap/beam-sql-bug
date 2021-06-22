51186f2f074a9868fcae8bffe364a320452c5e63
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2021-06-23 00:01:39 +0200

    Failing version with stack.

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for testsql
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Data.Int (Int32)
+import Database.PostgreSQL.Simple
+import Data.Foldable (for_)
+import Database.Beam
+import Database.Beam.Postgres
+import Data.Decimal
+import           Database.PostgreSQL.Simple.FromField
+import qualified Data.Attoparsec.ByteString.Char8 as AT
+import qualified Data.ByteString.Char8 as B8
+
+data Price = Price {
+    pValue :: Decimal
+  , pCurrencyId :: Int32
+} deriving (Show, Eq)
+
+instance FromBackendRow Postgres Price
+instance FromField Price where
+  fromField f bsval =
+    case bsval of
+      Nothing -> err UnexpectedNull "Null?"
+      Just dval -> do
+        tn <- typename f
+        case tn of
+          "price" ->
+            case AT.parseOnly parser dval of
+              Left msg -> err ConversionFailed msg
+              Right val -> return val
+          _ -> err Incompatible "Incompatible type"
+    where
+      parser = do
+        _ <- AT.char '('
+        amount <- AT.rational
+        _ <- AT.char ','
+        curid <- AT.decimal
+        _ <- AT.char ')'
+        return $ Price amount curid
+
+      err errC msg = do
+        typnam <- typename f
+        conversionError $ errC (B8.unpack typnam)
+                    (tableOid f)
+                    (maybe "Name" B8.unpack (name f))
+                    "price"
+                    msg
+
+data InternalPaymentT f = InternalPayment {
+    ipPaymid :: C f Int32
+  , ipPaymamount :: C f Price
+} deriving (Generic, Beamable)
+type InternalPayment = InternalPaymentT Identity
+deriving instance Show InternalPayment
+instance Table InternalPaymentT where
+  data PrimaryKey InternalPaymentT f = InternalPaymentId (Columnar f Int32) deriving (Generic, Beamable)
+  primaryKey = InternalPaymentId . ipPaymid
+
+data TrackingDb f = TrackingDb {
+    _dbInternalPayments :: f (TableEntity InternalPaymentT)
+} deriving (Generic, Database be)
+
+trackingDb :: DatabaseSettings Postgres TrackingDb
+trackingDb = defaultDbSettings
+
+
+main :: IO ()
+main = do
+  conn <- connectPostgreSQL "dbname=tracking"
+  pmlist <- runBeamPostgres conn $ do
+     runSelectReturningList $ select $ do
+                all_ (_dbInternalPayments trackingDb)
+  for_ pmlist $ \payment -> do
+    glist <- runBeamPostgres conn $ do
+        runSelectReturningList $ select $ do
+            lst <- all_ (_dbInternalPayments trackingDb)
+            pure (ipPaymid lst)
+    for_ glist $ \(_ :: Int32) -> do
+      return ()
+    liftIO $ print payment
